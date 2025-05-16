46d5325f07da2457e6144aed8198cc4561f0cbb3
Sohaib-2310 <muhammadsohaib8008@gmail.com>
2025-05-17 02:11:13 +0500

    Nav is under work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import './App.css'
-import LandingPg from './pages/landing/LandingPg'
+import React from "react";
+import "./App.css";
+import LandingPg from "./pages/landing/LandingPg";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
-      < LandingPg />
+      <LandingPg />
+      < Navbar />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import './Navbar.css'
+import { FaUser, FaSearch } from "react-icons/fa";
+import { PiShoppingCartSimpleFill } from "react-icons/pi";
 
 const Navbar = () => {
   return (
     <>
+      <nav className="flex justify-between items-center mx-12 w-auto border border-amber-600">
+        <div className="logo">Foodie</div>
+
+        <div className="menu-items flex">
+          <ul className="flex items-center">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Book Table</a></li>
+          </ul>
+
+          <ul className="flex items-center">
+            <li><a href="#"><FaUser /></a></li>
+            <li><a href="#"><PiShoppingCartSimpleFill /></a></li>
+            <li><a href="#"><FaSearch /></a></li>
+            <li><button>Order Online</button></li>
+          </ul>
+        </div>
+      </nav>
     </>
   )
 }
 
 export default Navbar
+
